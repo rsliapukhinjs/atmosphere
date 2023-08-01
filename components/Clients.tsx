c524e1aa048b58d524ea221bd2b2cdef3c4db3df
rsliapukhinjs <rsliapukhinjs@gmail.com>
2023-08-01 12:53:36 +0300

    minor changes

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -12,7 +12,7 @@ import sol from "../public/clients/sol.png";
 const Clients = () => {
   return (
     <section className="hidden md:flex flex-col items-center gap-8 p-8 text-sky-800 bg-sky-50">
-      <h2 className="text-xl">Наши клиенты</h2>
+      <h2 className="text-2xl">Наши клиенты</h2>
       <article className="w-2/3 grid grid-cols-2 lg:grid-cols-4 gap-4">
         <div className="flex items-center justify-center p-4">
           <Image
--- components/Cta.tsx
+++ components/Cta.tsx
@@ -1,9 +1,15 @@
-import { FaEnvelope, FaPhoneSquare, FaLocationArrow } from "react-icons/fa";
+import {
+  FaEnvelope,
+  FaPhoneSquare,
+  FaLocationArrow,
+  FaPrint,
+} from "react-icons/fa";
 
 const Cta = () => {
   return (
-    <section className="w-full flex flex-col lg:grid lg:grid-cols-2 gap-8 p-8  ">
-      <form className="p-8 flex flex-col items-start gap-2 rounded-lg text-sm lg:text-md border-2 text-sky-50 bg-sky-800">
+    <section className="w-full flex flex-col lg:grid lg:grid-cols-[2fr,1fr] gap-8 p-8  ">
+      <form className=" p-8 flex flex-col items-start gap-2 rounded-lg text-sm lg:text-md border-2 text-sky-50 bg-sky-800">
+        <h2 className="text-2xl">Свяжитесь с нами сейчас!</h2>
         <div className="flex flex-col gap-2 w-full">
           <label htmlFor="name">Ваше имя:</label>
           <input type="text" id="name" className="opacity-70 text-sky-800" />
@@ -39,21 +45,25 @@ const Cta = () => {
           Связаться
         </button>
       </form>
-      <div className="flex md:flex-col justify-around md:justify-center gap-4 lg:gap-16 bg-emerald-50 text-2xl p-8 text-sky-800 rounded-lg">
+      <div className="flex md:flex-col justify-around md:justify-center gap-8 text-2xl p-8 text-emerald-800 rounded-lg bg-emerald-50">
         <div className="flex items-center gap-4 cursor-pointer">
-          <FaLocationArrow className="inline" size="50" />
+          <FaLocationArrow className="inline" size="30" />
           <span className="hidden md:inline">
-            г. Гомель, ул. Б. Хмельницкого, д.79, 3-й этаж
+            г. Гомель, ул. Б. Хмельницкого, д.79, 3-ий этаж
           </span>
         </div>
         <div className="flex items-center gap-4 cursor-pointer">
-          <FaPhoneSquare className="inline" size="50" />
+          <FaPhoneSquare className="inline" size="30" />
           <span className="hidden md:inline">
-            8-(0232)-56-14-75, +375-(44)-702-30-93
+            8-(0232)-56-14-71, 8-(0232)-56-14-78
           </span>
         </div>
-        <div className="flex items-center gap-8 cursor-pointer">
-          <FaEnvelope className="inline" size="50" />
+        <div className="flex items-center gap-4 cursor-pointer">
+          <FaPrint className="inline" size="30" />
+          <span className="hidden md:inline">8-(0232)-56-14-78 (тел/факс)</span>
+        </div>
+        <div className="flex items-center gap-4 cursor-pointer">
+          <FaEnvelope className="inline" size="30" />
           <span className="hidden md:inline lowercase">
             atmosfera_gomel@mail.ru
           </span>
